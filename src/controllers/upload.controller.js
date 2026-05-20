fac4678498f6f8fcc8237df906f8a4b5212e8ea7
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-05-20 23:05:29 +0530

    feat: implement uploadPdf controller for handling PDF uploads

---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,25 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import { uploadPdfService } from "../services/upload.service.js";
+import ApiResponse from "../utils/apiResponse.js";
+import ApiError from "../utils/ApiError.js";
+
+export const uploadPdf = asyncHandler(
+  async (req, res) => {
+    if (!req.file) {
+      throw new ApiError(400, "PDF file is required");
+    }
+
+    const pdf = await uploadPdfService({
+      file: req.file,
+      userId: req.user.id,
+    });
+
+    return res.status(201).json(
+      new ApiResponse(
+        201,
+        pdf,
+        "PDF uploaded successfully"
+      )
+    );
+  }
+);
