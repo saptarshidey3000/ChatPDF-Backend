9d7c22bcd1daa469c0e006ce42e0a6f5c853a811
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-05-21 18:22:44 +0530

    implement chat completion service using OpenAI API

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -25,4 +25,42 @@ export const generateEmbedding = async (text) => {
         console.error("Error generating embedding:", error);
         throw new Error("Failed to generate embedding");
     }
+};
+/*
+|--------------------------------------------------------------------------
+| CHAT COMPLETION
+|--------------------------------------------------------------------------
+|
+| This generates AI responses.
+|
+| Model:
+| GPT-4.1-mini
+|
+*/
+export const generateChatCompletion = async (
+systemPrompt,
+userMessage,
+) => {
+    try {
+        //sending conversation to openai to generate a response
+        const response = await openai.chat.completions.create({
+            model: "gpt-4.1-mini",
+            messages: [
+                {
+                    role: "system",
+                    content: systemPrompt,
+                },
+                {
+                    role: "user",
+                    content: userMessage,
+                },
+            ] ,
+            //lowering the temperature makes the output more deterministic
+            temperature: 0.2,
+        });
+        return response.choices[0].message;
+    } catch (error) {
+        console.error("Error generating chat completion:", error);
+        throw new Error("Failed to generate chat completion");
+    }
 }
