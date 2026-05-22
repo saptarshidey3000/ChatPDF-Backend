38c02f3f0621a30eb59c01f46ea64114d021e583
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-05-22 23:25:33 +0530

    implement streamPdfAnswer function for real-time PDF AI responses

---
+++ src/controllers/stream.controller.js
@@ -0,0 +1,52 @@
+import {streamRagResponse} from "../services/ai.service.js";
+
+//Stream PDFAI response
+export const streamPdfAnswer = async (req, res) => {
+    try{
+        const {
+            pdfId, question,
+        } = req.body;
+        //Validate input
+        if (!pdfId || !question) {
+            return res.status(400).json({
+                success: false,
+                message: "pdfId and question are required",
+            });
+        }
+         /*
+      |--------------------------------------------------------------------------
+      | SSE Headers
+      |--------------------------------------------------------------------------
+      |
+      | Keeps connection open
+      | Allows real-time streaming
+      |
+      */
+        res.setHeader(
+            "Content-Type",
+            "text/event-stream"
+        );
+        res.setHeader(
+            "Cache-Control",
+            "no-cache"
+        );
+        res.setHeader(
+            "Connection",
+            "keep-alive"
+        );
+        //Strart streaming RAG response
+        await streamRagResponse({
+            pdfId,
+            question,
+            userId: req.userId,
+            res,
+        });
+    }
+    catch (error) {
+        console.error("Error in streamPdfAnswer:", error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to stream PDF answer",
+        });
+    }
+};
