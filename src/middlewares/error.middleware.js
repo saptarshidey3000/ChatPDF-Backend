2aaf3d6aa1fdc5569d1a91be62698064b57456a5
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-05-24 02:42:43 +0530

    enhance error middleware

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,9 +1,29 @@
-const errorMiddleware = (err, req, res, next) => {
+const errorMiddleware = (
+  err,
+  req,
+  res,
+  next
+) => {
+
+  console.error("ERROR:");
   console.error(err);
 
-  return res.status(err.statusCode || 500).json({
+  console.error("STACK:");
+  console.error(err.stack);
+
+  return res.status(
+    err.statusCode || 500
+  ).json({
     success: false,
-    message: err.message || "Internal Server Error",
+    message:
+      err.message ||
+      "Internal Server Error",
+
+    stack:
+      process.env.NODE_ENV ===
+      "development"
+        ? err.stack
+        : undefined,
   });
 };
 
