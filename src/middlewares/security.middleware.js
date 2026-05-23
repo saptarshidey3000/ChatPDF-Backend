a078e684f11200b41e3e6610f3645d7244dc4e6f
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-05-23 16:54:22 +0530

    improve security middleware comments and formatting

--- src/middlewares/security.middleware.js
+++ src/middlewares/security.middleware.js
@@ -1,22 +1,19 @@
 import helmet from "helmet";
 import hpp from "hpp";
 import compression from "compression";
-import xss from "xss-clean";
-import mongoSanitize from "express-mongo-sanitize";
 
-//Security Middlewares
+// Security Middlewares
 const securityMiddleware = (app) => {
-    //Set security HTTP headers
-    app.use(helmet());
 
-    //Prevent HTTP Parameter Pollution
-    app.use(hpp());
-    //Compress responses
-    app.use(compression());
-    //Data sanitization against XSS  
-    app.use(xss());
-    //Data sanitization against NoSQL injection
-    app.use(mongoSanitize());
+  // Secure HTTP headers
+  app.use(helmet());
+
+  // Prevent HTTP Parameter Pollution
+  app.use(hpp());
+
+  // Compress responses
+  app.use(compression());
+
 };
 
 export default securityMiddleware;
