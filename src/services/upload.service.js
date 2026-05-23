771729dc35d31ef769f182fd0c498c6b9516b1d6
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-05-24 02:09:48 +0530

    multer bug fixed

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -1,24 +1,71 @@
 import prisma from "../config/db.js";
+
 import { utapi } from "../config/uploadthing.js";
 
 export const uploadPdfService = async ({
   file,
   userId,
 }) => {
-  // Upload to UploadThing
 
-  const uploadedFile = await utapi.uploadFiles(file);
+  /*
+  |--------------------------------------------------
+  | Convert multer buffer to File
+  |--------------------------------------------------
+  */
 
-  // Save metadata to DB
+  const pdfFile = new File(
+    [file.buffer],
+    file.originalname,
+    {
+      type: file.mimetype,
+    }
+  );
+
+  /*
+  |--------------------------------------------------
+  | Upload to UploadThing
+  |--------------------------------------------------
+  */
+
+  const uploadedFile =
+    await utapi.uploadFiles(pdfFile);
+
+  /*
+  |--------------------------------------------------
+  | Upload failed
+  |--------------------------------------------------
+  */
+
+  if (!uploadedFile.data) {
+    throw new Error(
+      "Failed to upload PDF"
+    );
+  }
+
+  /*
+  |--------------------------------------------------
+  | Save metadata to DB
+  |--------------------------------------------------
+  */
 
   const pdf = await prisma.pdf.create({
     data: {
-      fileName: uploadedFile.data.name,
-      originalName: file.originalname,
-      fileUrl: uploadedFile.data.url,
-      fileSize: file.size,
+      fileName:
+        uploadedFile.data.name,
+
+      originalName:
+        file.originalname,
+
+      fileUrl:
+        uploadedFile.data.url,
+
+      fileSize:
+        file.size,
+
       userId,
-      processingStatus: "PROCESSING",
+
+      processingStatus:
+        "PROCESSING",
     },
   });
 
