749f28909f6e5ada8e82ad9a386169cb83ae9a79
saptarshidey3000 <saptarshidey3000@gmail.com>
2026-05-24 02:27:28 +0530

    fix import casting

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import { uploadPdfService } from "../services/upload.service.js";
-import ApiResponse from "../utils/apiResponse.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiError.js";
 
 export const uploadPdf = asyncHandler(
